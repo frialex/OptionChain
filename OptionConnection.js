d5e6d3924fe2c399aa512909f0bc6433fdfcafc6
Frison Alexander <fri.b.alex@gmail.com>
2013-07-16 21:29:21 -0400

    simple bindings

--- OptionConnection.js
+++ OptionConnection.js
@@ -1,10 +1,32 @@
 function cbfunc(data){
-	console.log(data);
-	window.gd = data;
-	window.OpCh = data.query.results.optionsChain;
+   var result = data.query.results.optionsChain;
+   window.opCh = result;
 
+   //TODO: derive date from result.option.symbol
 
-	//Use D3
+   console.log(result);
+   console.log(_.keys(result.option));
+
+   $(function(){drawChainChart(result)});
+}
+
+function drawChainChart(data){
+   d3 .select('body').selectAll("div")
+      .data(data.option)
+      .enter()
+      .append("p")
+      .text(function(d){ return d.symbol })
+      .style('color', function(d){return colorOptionType(d)});
+
+}
+
+function colorOptionType(option){
+   if(option.type === 'C'){
+      return 'green';
+   }else if(option.type === 'P'){
+      return 'blue';
+   }
+}
 
 	//Data Format
    // "symbol":"COMPANY SYMBOL",
@@ -24,13 +46,9 @@ function cbfunc(data){
    //                "openInt":"4"
    //             }
 
-
-   //Pump that in to D3
-
    //Color Puts and Calls differently
 
    //Symbol contains date information: YY MM DD?
 
    //Need some kind of graphical way to represent "in the money"
 
-}
