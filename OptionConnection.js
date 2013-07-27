335a99d93fc8db7de17543a8d95ee976048ffb60
Frison Alexander <fri.b.alex@gmail.com>
2013-07-27 09:32:06 -0400

    updated readme. Plans for graph

--- OptionConnection.js
+++ OptionConnection.js
@@ -14,17 +14,44 @@ function drawChainChart(data){
    var svgBase =   d3 .select('body').append("svg").attr('width', 500).attr('height', 500);
 
    var baseGraphic = svgBase.selectAll('circle').data(data.option).enter().append('circle')
-                              .attr({cx: 0, cy: 0, r: 0});
+                              .attr({cx: _.random(0,200), cy: function(d){return initialOptionPlacement(d);}, r: 0});
 
    //TODO: Need bette placement than _.random... //TODO: .ease('circle')? bounce?
-   var extendedGraphic =   baseGraphic.transition().duration(700)
-                                      .attr('cx', function(d, i){ return (i*10 + _.random(20,50)); }) 
+   var extendedGraphic =   baseGraphic.transition().duration(750)
+                                      .attr('cx', function(d, i){ return (i*10 + _.random(10,200)); }) 
                                       .attr('cy', function(d, i){ return placeOption(d); })
                                       .attr('r',  function(d)   { return d.strikePrice/10; });
 
    extendedGraphic.attr('fill', function(d){ return colorOptionType(d); })
-   //TODO: Add stroke and stroke width?? what would that mean? how would you read it back?
 
+   //TODO: Add stroke and stroke width?? what would that mean? how would you read it back?
+   //Without the delay, the objects don't get resized or positioned..
+   setTimeout(function() {d3.selectAll('circle').transition().duration(1000).attr('stroke', 'orange');}, 700);
+
+}
+
+
+
+//draw horizontal axis with strike price, calls on top puts on bottom.
+//what does the vertical direction represent? ask/bid prices?
+
+
+// (call)                 (call)
+//
+//---10------30------40-------50------60---70
+//
+//                   ()
+
+
+//Turn it in to a class? _ merge methods?
+//Start at top or bottom, depending on option type.
+//Adds for a nice distinguishing effect. 
+function initialOptionPlacement(op){
+   if(op.type === 'C'){
+      return _.random(-10, 50);
+   }else if(op.type === 'P'){
+      return _.random(100,300);
+   }
 }
 
 function placeOption(op){
--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 OptionChain
 ===========
 
-Visualize options chains instead of viewing it as a table
+Visualize options chains.
+
+Running: 
+
+Install Node. CD to directory. Run: npm install
+
+After all the node packages are installed, run: node server.js
+
+Go to localhost:3000
+
+Right now, you can run the index.html without node. 
+
